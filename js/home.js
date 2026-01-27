e55a4acb7ee5ca02ec5582ffd6fb13b1b84579f8
Duczzzz <120049209+Duczzzz@users.noreply.github.com>
2026-01-28 01:28:25 +0700

    Add files via upload

--- js/home.js
+++ js/home.js
@@ -191,149 +191,223 @@ document.getElementById("btn-inout2").onclick = function () {
     btn.style.backgroundColor = "rgb(255, 152, 152)";
   }
 };
-var count = 0;
+var count = 4;
+const charts = {};
 document.getElementById("addblock").onclick = function () {
   let box = document.createElement("div");
   count += 1;
-  box.className = "box box5";
+  box.className = "box box" + count;
   box.innerHTML = `
     <h2>Thêm card mới</h2>
     <form>
-      <input type="text" placeholder="Nhập tên card" id = "cardName"><br>
+      <input type="text" placeholder="Nhập tên card" id = "cardName" required="required"><br>
       <label>Lựa chọn loại card</label>
       <select id="cardType">
+        <option value="" selected disabled>-- Chọn loại card --</option>
         <option value="Cảm biến">Cảm biến</option>
-        <option value="Điều khiển In Out">Điều khiển In Out</option>
-      </select><br>
-      <label>Lựa chọn loại biểu đồ</label>
-      <select id="chartType">
-        <option value="line">Biểu đồ đường</option>
-        <option value="bar">Biểu đồ cột</option>
-        <option value="pie">Biểu đồ tròn</option>
-        <option value="doughnut">Biểu đồ vòng</option>
-        <option value="mixchart">Biểu đồ hỗn hợp</option>
-        <option value="none">Không</option>
-      </select><br>
-      <label>Lựa chọn chân kết nối</label>
-      <select id="selectPin">
-        <option value="2">GPIO2</option>
-        <option value="4">GPIO4</option>
-        <option value="5">GPIO5</option>
-        <option value="16">GPIO16</option>
-        <option value="17">GPIO17</option>
-        <option value="18">GPIO18</option>
-        <option value="19">GPIO19</option>
-        <option value="21">GPIO21</option>
-        <option value="22">GPIO22</option>
-        <option value="23">GPIO23</option>
-        <option value="25">GPIO25</option>
-        <option value="26">GPIO26</option>
-        <option value="27">GPIO27</option>
-        <option value="32">GPIO32</option>
-        <option value="33">GPIO33</option>
-      </select><br>
+        <option value="control">Điều khiển In Out</option>
+      </select>
+      <br>
+      <div class="chartSelect" style="display:none;">
+        <label>Lựa chọn loại biểu đồ</label>
+        <select id="chartType">
+          <option value="line">Biểu đồ đường</option>
+          <option value="bar">Biểu đồ cột</option>
+          <option value="pie">Biểu đồ tròn</option>
+          <option value="doughnut">Biểu đồ vòng</option>
+          <option value="mixchart">Biểu đồ hỗn hợp</option>
+          <option value="none">Không</option>
+        </select>
+      </div>
+      <br>
+      <div class="PinSelect" style="display:none;">
+        <label>Lựa chọn chân kết nối</label>
+        <select id="selectPin">
+          <option value="2">GPIO2</option>
+          <option value="4">GPIO4</option>
+          <option value="5">GPIO5</option>
+          <option value="16">GPIO16</option>
+          <option value="17">GPIO17</option>
+          <option value="18">GPIO18</option>
+          <option value="19">GPIO19</option>
+          <option value="21">GPIO21</option>
+          <option value="22">GPIO22</option>
+          <option value="23">GPIO23</option>
+          <option value="25">GPIO25</option>
+          <option value="26">GPIO26</option>
+          <option value="27">GPIO27</option>
+          <option value="32">GPIO32</option>
+          <option value="33">GPIO33</option>
+        </select>
+      </div>
+      <br>
+      <div class="Pin2Select" style="display:none;">
+        <label>Lựa chọn chân kết nối2</label>
+        <select id="selectPin2">
+          <option value="2">GPIO2</option>
+          <option value="4">GPIO4</option>
+          <option value="5">GPIO5</option>
+          <option value="16">GPIO16</option>
+          <option value="17">GPIO17</option>
+          <option value="18">GPIO18</option>
+          <option value="19">GPIO19</option>
+          <option value="21">GPIO21</option>
+          <option value="22">GPIO22</option>
+          <option value="23">GPIO23</option>
+          <option value="25">GPIO25</option>
+          <option value="26">GPIO26</option>
+          <option value="27">GPIO27</option>
+          <option value="32">GPIO32</option>
+          <option value="33">GPIO33</option>
+        </select>
+      </div>
+      <br>
       <button type="button" id="getInfor">Xác nhận</button>
     </form>
   `;
+  const cardTypeSelect = box.querySelector("#cardType");
+  const selectPin2 = box.querySelector(".Pin2Select");
+  const selectChart = box.querySelector(".chartSelect");
+  const selectPin = box.querySelector(".PinSelect");
+  cardTypeSelect.addEventListener("change", () => {
+    if (cardTypeSelect.value === "Cảm biến") {
+      selectPin2.style.display = "none";
+      selectChart.style.display = "block";
+      selectPin.style.display = "block";
+    } else {
+      selectPin2.style.display = "block";
+      selectChart.style.display = "none";
+      selectPin.style.display = "block";
+    }
+  });
   document.querySelector(".container").appendChild(box);
   box.querySelector("#getInfor").onclick = function () {
     const selectChart = box.querySelector("#chartType").value;
     const selectCard = box.querySelector("#cardType").value;
     const selectPin = box.querySelector("#selectPin").value;
-    const cardName = box.querySelector("#cardName").value;
+    let cardName = box.querySelector("#cardName").value;
+    if (cardName == "") {
+      cardName = "test" + count;
+    }
     if (selectChart == "pie" || selectChart == "doughnut") {
       alert(
         "tính năng đang được phát triển \n bạn vui lòng chọn loại biểu đồ khác",
       );
       return;
     }
-    alert(
-      `Bạn đã chọn:
-      Biểu đồ: ${selectChart}
+    if (selectCard == "Cảm biến") {
+      alert(
+        `Bạn đã chọn:
       Card: ${selectCard}
+      Biểu đồ: ${selectChart}
       GPIO: ${selectPin}`,
-    );
-    box.innerHTML = `
-    <h1 class="heading">${cardName}</h1>
-    <h2>Loại card: ${selectCard}</h2>
-    <h2>Chân kết nối: GPIO${selectPin}</h2>
-    <h2>Loại biểu đồ: ${selectChart}</h2>
-    ${
-      selectChart !== "none"
-        ? `<div class="card-chart">
-             <canvas id="Chart${count}"></canvas>
-           </div>`
-        : ""
-    }
-  `;
-    if (selectChart != "none") {
-      const ctxNew = document.getElementById(`Chart${count}`);
-      if (selectChart == "line") {
-        const mixedChart1 = new Chart(ctxNew, {
-          data: {
-            datasets: [
-              {
-                type: "line",
-                label: "Độ ẩm",
-                data: [30, 40, 50, 100],
-              },
-            ],
-            labels: [0, 1, 2, 3],
-          },
-          options: {
-            scales: {
-              y: {
-                beginAtZero: true,
+      );
+      box.innerHTML = `
+        <h1 class="heading">${cardName}</h1>
+        <h2>Loại card: ${selectCard}</h2>
+        <h2>Chân kết nối: GPIO${selectPin}</h2>
+        <h2>Loại biểu đồ: ${selectChart}</h2>
+        ${
+          selectChart !== "none"
+            ? `<div class="card-chart">
+                <canvas id="Chart${count}"></canvas>
+              </div>`
+            : ""
+        }
+      `;
+      if (selectChart != "none") {
+        const ctxNew = document.getElementById(`Chart${count}`);
+        if (selectChart == "line") {
+          charts[count] = new Chart(ctxNew, {
+            data: {
+              datasets: [
+                {
+                  type: "line",
+                  label: "Độ ẩm",
+                  data: [30, 40, 50, 100],
+                },
+              ],
+              labels: [0, 1, 2, 3],
+            },
+            options: {
+              scales: {
+                y: {
+                  beginAtZero: true,
+                },
               },
             },
-          },
-        });
-      } else if (selectChart == "bar") {
-        const mixedChart1 = new Chart(ctxNew, {
-          data: {
-            datasets: [
-              {
-                type: "bar",
-                label: "Nhiệt độ",
-                data: [10, 30, 100, 40, 50],
-              },
-            ],
-            labels: [0, 1, 2, 3, 4],
-          },
-          options: {
-            scales: {
-              y: {
-                beginAtZero: true,
+          });
+        } else if (selectChart == "bar") {
+          charts[count] = new Chart(ctxNew, {
+            data: {
+              datasets: [
+                {
+                  type: "bar",
+                  label: "Nhiệt độ",
+                  data: [10, 30, 100, 40, 50],
+                },
+              ],
+              labels: [0, 1, 2, 3, 4],
+            },
+            options: {
+              scales: {
+                y: {
+                  beginAtZero: true,
+                },
               },
             },
-          },
-        });
-      } else if (selectChart == "mixchart") {
-        const mixedChart1 = new Chart(ctxNew, {
-          data: {
-            datasets: [
-              {
-                type: "bar",
-                label: "Nhiệt độ",
-                data: [10, 30, 40, 100],
-              },
-              {
-                type: "line",
-                label: "Độ ẩm",
-                data: [40, 50, 10, 40],
-              },
-            ],
-            labels: [0, 1, 2, 3],
-          },
-          options: {
-            scales: {
-              y: {
-                beginAtZero: true,
+          });
+        } else if (selectChart == "mixchart") {
+          charts[count] = new Chart(ctxNew, {
+            data: {
+              datasets: [
+                {
+                  type: "bar",
+                  label: "Nhiệt độ",
+                  data: [10, 30, 40, 100],
+                },
+                {
+                  type: "line",
+                  label: "Độ ẩm",
+                  data: [40, 50, 10, 40],
+                },
+              ],
+              labels: [0, 1, 2, 3],
+            },
+            options: {
+              scales: {
+                y: {
+                  beginAtZero: true,
+                },
               },
             },
-          },
-        });
+          });
+        }
       }
+    } else if (selectCard == "control") {
+      const selectPin2 = box.querySelector("#selectPin2").value;
+      if (selectPin == selectPin2) {
+        alert("vui lòng thay đổi chân GPIO");
+        return;
+      }
+      alert(
+        `Bạn đã chọn:
+      Card: ${selectCard}
+      GPIO: ${selectPin}
+      GPIO2: ${selectPin2}`,
+      );
+      box.innerHTML = `
+        <h1 class="heading">${cardName}</h1>
+        <h2>Loại card: ${selectCard}</h2>
+        <h2>Chân kết nối: GPIO${selectPin}</h2>
+        <h2>Chân kết nối2: GPIO${selectPin2}</h2>
+        <div class="button_group">
+          <button id="btn-inout3">IN 1</button>
+          <p id="status1">OUT 1 Đang tắt</p>
+          <button id="btn-inout4">IN 2</button>
+          <p id="status2">OUT 2 Đang tắt</p>
+        </div>
+      `;
     }
   };
 };
@@ -344,9 +418,7 @@ let live1 = false;
 document.getElementById("live").onclick = function () {
   live1 = !live1;
   const ele = document.getElementById("live");
-
   const zoomOpt = mixedChart2.options.plugins.zoom;
-
   if (live1) {
     zoomOpt.zoom.wheel.enabled = false;
     zoomOpt.zoom.pinch.enabled = false;
